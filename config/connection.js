1366b49dba5b1fb18368bc2c31d9d1c4e36e0bad
Daniel V <main@dvasquez4155.com>
2020-03-26 20:54:19 -0700

    Updated schemas

--- config/connection.js
+++ config/connection.js
@@ -6,7 +6,7 @@ var connection = mysql.createConnection({
     port: 3306,
     user: "root",
     password: "LK41m&kxVA6c8Mt@",
-    database: "burger_db"
+    database: "burgers_db"
 });
 
 // Make connection.
